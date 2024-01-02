3a593d8d703ac5d9c96867d4f8acf8446990b0b6
codigospythonrk <155478213+codigospythonrk@users.noreply.github.com>
2024-01-02 15:30:13 -0300

    Update 001.py

--- 001.py
+++ 001.py
@@ -3,6 +3,8 @@ from tkinter import *
 class App:
     def __init__(self):
         self.janela = Tk()
+        self.janela.title('Minha Primeira Janela')
+        self.janela.geometry('300x150')
         self.janela.mainloop()
 
 aplicacao=App()
